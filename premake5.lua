28fce48e40f092909e20faecb8f3cde012edb83d
Rahul <rahul.jhugroo99@gmail.com>
2024-05-22 20:32:11 +1000

    update build process

--- premake5.lua
+++ premake5.lua
@@ -1,30 +1,10 @@
--- premake5.lua
-workspace "opengles-renderer"
-	configurations { "debug", "release" }
-	location "build"
+local mergeTables = function(into, from)
+	for _,v in ipairs(from) do
+		table.insert(into, v)
+	end
 
-local projectName = "opengles-renderer"
-project(projectName)
-	kind "ConsoleApp"
-	language "C++"
-	targetdir "bin/%{cfg.buildcfg}"
-	location("build/" .. projectName)
-
-	files { "src/*.h", "src/*.hpp", "src/*.c", "src/*.cpp" }
-	includedirs { "include" }
-
-	libdirs { "lib" }
-	links { "mingw32", "SDL2main", "SDL2", "EGL", "GLESv2" }
-
-	filter "configurations:debug"
-		defines { "DEBUG" }
-		symbols "On"
-
-	filter "configurations:release"
-		defines { "NDEBUG" }
-		optimize "Speed"
-
-		
+	return into
+end
 
 -- Clean Function --
 newaction {
@@ -36,3 +16,60 @@ newaction {
 	   print("done.")
 	end
  }
+
+newoption {
+	trigger = "physics",
+	value = "backend",
+	description =  "Choose a backend for the physics engine.",
+	allowed = {
+		{ "none", "No physics engine..."},
+		{ "jolt", "Jolt physics"},
+	},
+	default = "jolt",
+}
+
+local physTable = {
+	["none"] = "PHYSICS_NONE",
+	["jolt"] = "PHYSICS_JOLT",
+}
+
+
+-- premake5.lua
+workspace("opengles-renderer")
+	configurations( { "debug", "release" } )
+	location("build")
+
+local projectName = "opengles-renderer"
+project(projectName)
+	kind("ConsoleApp")
+	language("C++")
+	targetdir("bin/%{cfg.buildcfg}")
+	location("build/" .. projectName)
+
+	files( {"src/*.h", "src/*.c", "src/*.hpp", "src/*.cpp" } )
+	includedirs( { "include" } )
+
+	libdirs( {"lib" } )
+	links( {"mingw32", "SDL2main", "SDL2", "EGL", "GLESv2", "Jolt" } )
+	
+	-- filter("action:gmake2")
+	-- 	links( { "mingw32" })
+	-- filter({ })
+
+	-- physics implementation
+	defines( { physTable[_OPTIONS["physics"]] })
+	defines( { "ENABLE_DEBUG_RENDER", "JPH_DEBUG_RENDERER", "JPH_PROFILE_ENABLED" } )
+
+	-- filter("options:not nodebugrender")
+
+	-- configurations
+	filter("configurations:debug")
+		defines( { "DEBUG", "JPH_ENABLE_ASSERTS" })
+		symbols("On")
+
+	filter("configurations:release")
+		defines( { "NDEBUG" })
+		optimize("Speed")
+
+	flags( { "Verbose" } )
+
